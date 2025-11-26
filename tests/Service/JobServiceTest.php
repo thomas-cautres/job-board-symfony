2f53370b37651316d4b6df1fe5a1bb6194154612
Thomas Cautrès <thomas.cautres@protonmail.com>
2025-11-26 22:29:31 +0100

    --

--- tests/Service/JobServiceTest.php
+++ tests/Service/JobServiceTest.php
@@ -7,12 +7,9 @@ namespace App\Tests\Service;
 use App\Dto\Job\CreateJobDto;
 use App\Entity\Company;
 use App\Entity\Job;
-use App\Entity\Recruiter;
 use App\Service\JobService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class JobServiceTest extends KernelTestCase
 {
@@ -24,10 +21,6 @@ class JobServiceTest extends KernelTestCase
         self::bootKernel();
         $this->jobService = self::getContainer()->get(JobService::class);
         $this->entityManager = self::getContainer()->get(EntityManagerInterface::class);
-
-        $user = $this->entityManager->getRepository(Recruiter::class)->findOneBy(['email' => 'recruiter@my-company.com']);
-        $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
-        self::getContainer()->get(TokenStorageInterface::class)->setToken($token);
     }
 
     public function testCreate(): void
@@ -52,6 +45,6 @@ class JobServiceTest extends KernelTestCase
         $this->assertSame($dto->title, $createdJob->getTitle());
         $this->assertSame($dto->description, $createdJob->getDescription());
         $this->assertSame($company, $createdJob->getCompany());
-        $this->assertSame('recruiter@my-company.com', $createdJob->getCreatedBy()->getEmail());
+        $this->assertNull($createdJob->getCreatedBy());
     }
 }
